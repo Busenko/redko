6a12674b155344acd7385cc0d3948f330f1d1b2a
Ivan Busenko <132225898+Busenko@users.noreply.github.com>
2024-12-17 02:05:26 +0200

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,110 @@
+window.addEventListener("DOMContentLoaded", function () {
+
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+    
+            const targetId = this.getAttribute('href'); 
+            const targetElement = document.querySelector(targetId); 
+    
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop - 50, 
+                    behavior: 'smooth' 
+                });
+            }
+        });
+    });
+
+    
+    window.addEventListener('load', function() {
+        const content = document.getElementById('content');
+       
+        setTimeout(() => {
+          content.classList.add('show');
+        }, 100); 
+      });
+      
+
+
+    // Меню бургер...................................................................................................................
+ // Меню бургер...................................................................................................................
+$(document).ready(function () {
+    const burgerMenu = document.querySelector('.menu__burger');
+    const menuBlock = document.querySelector('.menu__block');
+    const menuLinks = document.querySelectorAll('.menu__link');
+    
+    if (burgerMenu && menuBlock) {
+        // Відкривання/закривання меню при кліку на бургер
+        $(burgerMenu).click(function () {
+            $(this).toggleClass('active');
+            $(menuBlock).toggleClass('active');
+            $('body').toggleClass('lock');
+        });
+
+        // Закриття меню при кліку на посилання
+        menuLinks.forEach(link => {
+            $(link).click(function () {
+                $(burgerMenu).removeClass('active'); // Видаляємо клас "active" у бургер-іконки
+                $(menuBlock).removeClass('active');  // Видаляємо клас "active" у меню
+                $('body').removeClass('lock');       // Розблоковуємо прокручування
+            });
+        });
+    }
+});
+
+
+    // Функція для оновлення висоти футера та хедера в CSS...................................................................................................................
+    function updateFooterAndHeaderHeight() {
+        const footer = document.querySelector('footer');
+        const header = document.querySelector('header');
+
+        if (footer && header) {
+            const footerHeight = footer.offsetHeight;
+            const headerHeight = header.offsetHeight;
+            document.documentElement.style.setProperty('--footer-height', `${footerHeight}px`);
+            document.documentElement.style.setProperty('--header-height', `${headerHeight}px`);
+        }
+    }
+
+    window.addEventListener('load', updateFooterAndHeaderHeight);
+    window.addEventListener('resize', updateFooterAndHeaderHeight);
+// робота з відкриваючими блоками...................................................................................................................
+    function itemsControl() {
+        const coll = document.getElementsByClassName('content__item');
+        if (coll.length > 0) {
+            for (let i = 0; i < coll.length; i++) {
+                const title = coll[i].querySelector('.item__button');
+                if (title) {
+                    title.addEventListener('click', function () {
+                        let content = coll[i].querySelector('.content__item__info');
+                        let image = coll[i].querySelector('.img-rotate');
+                        if (content) {
+                            if (content.style.maxHeight) {
+                                content.style.maxHeight = null;
+                                image && image.classList.remove('rotated');
+                            } else {
+                                content.style.maxHeight = content.scrollHeight + 'px';
+                                image && image.classList.add('rotated');
+                            }
+                        }
+                    });
+                }
+            }
+    
+            
+            window.addEventListener('resize', function () {
+                for (let i = 0; i < coll.length; i++) {
+                    const content = coll[i].querySelector('.content__item__info');
+                    if (content && content.style.maxHeight) {
+                        content.style.maxHeight = content.scrollHeight + 'px';
+                    }
+                }
+            });
+        }
+    }
+    
+    itemsControl();
+    
+
+});
